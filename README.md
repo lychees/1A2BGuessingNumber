6dc8cc677f7221d8a5535adf06dd1f731e08cd04
xiaodao <lychees67@gmail.com>
2018-01-10 14:19:25 +0800

    Go

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 1A2BGuessingNumber
+
+Online guessing number game in Ethereum.
+
+# Refs
+
+- www.spoj.com/problems/GUESSING/
+- https://kovan.etherscan.io/address/0xb3328d7587e8b8492fa937d9da5bb451d8d623f5#code
+- https://medium.com/taipei-ethereum-meetup/大撒幣-第一次玩智能合約就上手-4d86fa0b4957
+- https://www.youtube.com/watch?v=zGO7EQuSwu8
---
+++ contracts/Guess.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.19;
+contract guess {
+    mapping(uint => uint) public answer;
+    uint public seed = 2569;
+
+    event NewGame();
+    event Guess(uint A, uint B, uint C, uint D);
+    event WrongAnswer(uint A, uint B);
+    event Accepted(uint reward);
+
+	// create a four-digit random number
+	function newGame() {
+		seed = block.timestamp+block.difficulty+block.number + 256;
+
+		for (uint i=0;i<4;i++) {
+            uint x = seed % 9;
+            seed /= 10;
+        	answer[i] = x+1;
+		}
+		NewGame();
+	}
+
+	function guessnumber() {
+		newGame();
+	}
+
+	// enter a four-digit number and get a hint
+	// A = count of digits that meet the right number
+	// B = count of numbers that exist but at a wrong place
+	// eg: if the answer is 1234, then 3254 = 2A1B
+	function guess(uint _answer) payable returns(uint A,uint B){
+		require(msg.value >= 10);
+
+		uint[] memory guessanswer = new uint[](4);
+		uint a = 0;
+		uint b = 0;
+		for (uint i=0; i<4; i++) {
+			guessanswer[i] = _answer % 10;
+			_answer /= 10;
+		}
+		Guess(guessanswer[0], guessanswer[1], guessanswer[2], guessanswer[3]);
+		for (uint j=0;j<4;++j) {
+			if (guessanswer[j] == answer[j]) {
+				a += 1;
+			}
+			for (uint k=0;k<4;++k) {
+				if (guessanswer[k] == answer[j]) {
+					b += 1;
+				}
+			}
+		}
+
+		b -= a;
+		if (a == 4) {
+		    newGame();
+            Accepted(this.balance / 2);
+		    msg.sender.transfer(this.balance / 2);
+		} else {
+	    	WrongAnswer(a, b);
+		}
+		return (a,b);
+	}
+}
