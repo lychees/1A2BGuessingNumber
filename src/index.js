a59773474f74a24da127f7fbed35b4934b9ec0c9
stupid <sunfeng32@qq.com>
2018-01-15 03:28:01 +0800

    feat(App): use react-web3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Web3Provider } from 'react-web3';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Web3Provider>
+    <App />
+  </Web3Provider>
+), document.getElementById('root'));
 registerServiceWorker();
